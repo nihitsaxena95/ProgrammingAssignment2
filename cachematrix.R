8b009b2db3f2b2750de6452222aa1b1ebe5e9976
Nihit Saxena <nihitsaxena95@gmail.com>
2018-09-07 15:53:51 +0530

    Smart Cache Inverse Comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below functions create an object that will store a matrix and caches its inverse matrix.
 
-## This will create  a matrix and will store its inverse
+## creates a special "matrix", which is
+## really a list containing a function to
+
+## 1.  set the value of the matrix
+## 2.  get the value of the matrix
+## 3.  set the value of the inverse
+## 4.  get the value of the inverse
+
 
 makeCacheMatrix <- function(x = matrix()) {
   print("called")
@@ -17,6 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
   list(set=set,get=get, getinv=getinv,setinv=setinv)
 }
 
+
+## The following function calculates inverse of the special "matrix"
+## created with the above function. However, it first checks to see if the
+## inverse has already been calculated. If so, it `get`s the inverse from the
+## cache and skips the computation. Otherwise, it calculates inverse of
+## the data and sets the value of inverse in the cache via the `setinv`
+## function.
 
 ## Pass the object returned by the above function
 
